f84c2eb243fead19a823a661c6e09cb858dcfa74
Vikrant-K-Thakur <vikrantkthakur123@gmail.com>
2024-10-08 19:37:56 +0530

    #change

---
+++ Musiclibrary.py
@@ -0,0 +1,15 @@
+music = {
+    "love":"https://www.youtube.com/watch?v=XfXgUZDCAoA0", # one love
+    "no":"https://www.youtube.com/watch?v=6RrEQJNZwPQ&pp=ygUHbm8gbG92ZQ%3D%3D", # no love
+    "company":"https://www.youtube.com/watch?v=GuGYYdwcPYw&pp=ygUHY29tcGFueQ%3D%3D", # company
+    "pasoori":"https://www.youtube.com/watch?v=5Eqb_-j3FDA0", # pasoori
+    "rollin":"https://youtu.be/RLhuPD2ASKE", # we rollin
+    "cap":"https://www.youtube.com/watch?v=PejQbGZraqg", # no cap $
+    "guess":"https://www.youtube.com/watch?v=Qg9LxRHLbAk&pp=ygUIaSBndXNzZXM%3D", # i guess $
+    "soul":"https://www.youtube.com/watch?v=k3ijQJjUbTs&pp=ygUSYXJpaml0IHNpbmdoIHNvbmdz", # soulmate
+    "sajni":"https://www.youtube.com/watch?v=k3g_WjLCsXM&list=RD0pWsCiBvLOk&index=36&pp=8AUB", # sajni
+    "apna":"https://youtu.be/ElZfdU54Cp8?list=RD0pWsCiBvLOk", # apna banale piya
+    "khairiyat":"https://youtu.be/hoNb6HuNmU0", # Khairiyat
+    "bekhayali":"https://www.youtube.com/watch?v=VOLKJJvfAbg", # Bekhayali
+    "husn":"https://www.youtube.com/watch?v=gJLVTKhTnog&pp=ygUEaHVzbg%3D%3D", # husn
+}
